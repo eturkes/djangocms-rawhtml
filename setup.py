b96cf16075f861daacac3f9f166f4c67a2255cfc
Emir Turkes <eturkes@bu.edu>
2020-10-07 20:03:27 +0100

    Try a different way of using `install_requires`

--- setup.py
+++ setup.py
@@ -7,10 +7,6 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
-REQUIREMENTS = [
-    'Django==1.11.11',
-]
-
 setup(
     name='djangocms-rawhtml',
     version='0.2',
@@ -26,7 +22,9 @@ setup(
     url='https://github.com/ketaro/djangocms-rawhtml',
     author='Nick Avgerinos',
     author_email='nicka@axcella.com',
-    install_requires=REQUIREMENTS,
+    install_requires=(
+        'Django==1.11.11',
+    ),
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
