247d66eead91edf508c7f2f9d016c476ae1ae53c
Emir Turkes <eturkes@bu.edu>
2020-10-07 19:54:47 +0100

    Specify requirements using different syntax

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
-INSTALL_REQUIRES = [
+REQUIREMENTS = [
     'Django==1.11.11',
 ]
 
